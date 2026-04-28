eb434449a4b162eef2e004d82f09e05d09782732
cauecalil <caue.26@hotmail.com>
2026-04-28 16:24:17 -0300

    feat(api-service): add initial API Service setup with Spring Boot, Eureka, and database configuration

---
+++ backend/api-service/src/test/java/com/peculytics/apiservice/ApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peculytics.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
